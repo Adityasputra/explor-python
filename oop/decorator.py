f8c5fa6fa9e40146a9cca30f2db08dcf2ab4e917
Aditya Saputra <aditsputra.cv@gmail.com>
2025-11-23 14:30:00 +0700

    docs: how to make decorator

---
+++ oop/decorator.py
@@ -0,0 +1,86 @@
+class Math:
+    
+    @staticmethod
+    def add(a, b):
+        return a + b
+    
+
+# print(Math.add(2, 2)) # Result 4
+
+class BankAccount:
+    no = ""
+    balance = 0
+    active = True
+    
+    def __init__(self, no, balance = 0):
+        self.no = no
+        self.balance = balance
+        
+    @classmethod
+    def disable(cls, no, balance = 0):
+        result = cls(no, balance)
+        result.active = False
+        return result
+    
+User1 = BankAccount("1", "2000")
+print("="*35)
+print(f"No\t: {User1.no}\nBalance\t: {User1.balance}\nActive\t: {"This account is active" if User1.active else "This account is inactive"}")
+print("="*35)
+
+print()
+
+User2 = BankAccount.disable("2", "5000")
+print("="*35)
+print(f"No\t: {User2.no}\nBalance\t: {User2.balance}\nActive\t: {"This account is active" if User2.active else "This account is inactive"}")
+print("="*35)
+
+print()
+
+# Studi Case for Class Method as Factory
+class User:
+    def __init__(self, name, age = 17):
+        self.name = name
+        self.age = age
+        
+    @classmethod
+    def self_registration(cls, value):
+        name, age = value.split(",")
+        age = int(age)
+        
+        if age <= 17:
+            raise ValueError("Registration available for age greater than 17 years")
+        
+        return cls(name, age)
+    
+user1 = User.self_registration("Aditya,20")
+
+width = 20
+print("=" * width)
+print(" Registration ".center(width, "="))
+print("=" * width)
+
+print(f"{'Name':<10}: {user1.name}")
+print(f"{'Age':<10}: {user1.age} years")
+
+print("=" * width)
+
+# Study Case Instance Method
+class Rectangle:
+    def __init__(self, s, d):
+        self.s = s
+        self.d = d
+        
+    def area_formula(self):
+        return self.s ** 2
+    
+    def circumference_formula(self):
+        return 4 * self.s
+    
+    def formula_area_of_a_diagonal(self):
+        return (self.d ** 2) / 2
+    
+task1 = Rectangle(2, 2)
+print(task1.area_formula())
+print(task1.circumference_formula())
+print(task1.formula_area_of_a_diagonal())
+
